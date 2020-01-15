190aaa88be09cbf41134ab976d6218521a2cc073
Lane <lane.clark@gov.bc.ca>
2020-01-14 22:46:47 -0800

    carried away

--- SenTweetBC.py
+++ SenTweetBC.py
@@ -1,6 +1,7 @@
-
+#!/usr/bin/env python
 
 import twitter
+import pandas as pd
 
 # initialize api instance
 twitter_api = twitter.Api(consumer_key='',
@@ -9,4 +10,20 @@ twitter_api = twitter.Api(consumer_key='',
                         access_token_secret='')
 
 # test authentication
-print(twitter_api.VerifyCredentials())
+#print(twitter_api.VerifyCredentials())
+
+def buildTestSet(search_keyword):
+    try:
+        tweets_fetched = twitter_api.GetSearch(search_keyword, count = 100)
+
+        print('Fetched ' + str(len(tweets_fetched)) + ' tweets for the term ' + search_keyword)
+
+        return [{'text': status.text, 'label':None} for status in tweets_fetched]
+    except:
+        print('Something went wrong...')
+        return None
+search_term = input("Enter a search keyword:")
+testDataSet = buildTestSet(search_term)
+
+# Print 5 tweets (JSON)
+print(testDataSet[0:4])
