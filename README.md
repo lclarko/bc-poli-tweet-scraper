11ed94d7ba55bef2b20b94b5ea17ccd47f4f54e2
Lane <lane.clark@gov.bc.ca>
2020-08-23 13:24:19 -0700

    Tidying up repo
    - deleting readme. Will rewrite

--- README.md
+++
@@ -1,5 +0,0 @@
-# SenTweetBC
-Visualized sentiment analysis of tweets geolocated in British Columbia, Canada. 
-
-# App Usage
-This app will perform sentiment analysis of tweets geolocated in British Columbia, Canada. When sentiment reaches a certain level, the associated keywords will be written to a table where further analysis and processing will then result in word clouds. The word clouds will depict popularity and sentiment via size and colour, respectively. 
--- SenTweetBC.py
+++ SenTweetBC.py
@@ -1,29 +1,36 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
+# Daily BC Politics Tweet Scraper
+# See crontab -l for job time
+
+import os
+import datetime
 import twitter
-import pandas as pd
+import tweepy
+import csv
+from tweepy.auth import OAuthHandler
 
-# initialize api instance
-twitter_api = twitter.Api(consumer_key='',
-                        consumer_secret='',
-                        access_token_key='',
-                        access_token_secret='')
+# Twitter API Keys / Authentication Method
 
-# test authentication
-#print(twitter_api.VerifyCredentials())
+consumer_key = os.environ.get("TWITTER_CONSUMER_KEY")
+consumer_secret = os.environ.get("TWITTER_CONSUMER_SECRET")
+access_token = os.environ.get("TWITTER_ACCESS_TOKEN")
+access_token_secret = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+api = tweepy.API(auth,wait_on_rate_limit=True)
+auth.set_access_token(access_token, access_token_secret)
 
-def buildTestSet(search_keyword):
-    try:
-        tweets_fetched = twitter_api.GetSearch(search_keyword, count = 100)
+# Other Variables
 
-        print('Fetched ' + str(len(tweets_fetched)) + ' tweets for the term ' + search_keyword)
+csvFile = open('bcpoli.csv', 'a')
+csvWriter = csv.writer(csvFile)
+date = (datetime.date.today() - datetime.timedelta(days=1))
 
-        return [{'text': status.text, 'label':None} for status in tweets_fetched]
-    except:
-        print('Something went wrong...')
-        return None
-search_term = input("Enter a search keyword:")
-testDataSet = buildTestSet(search_term)
+for tweet in tweepy.Cursor(api.search,q="#bcpoli",count=100,
+                           lang="en",
+                           since=date).items():
+    print (tweet.id, tweet.created_at, tweet.text)
+    csvWriter.writerow([tweet.id, tweet.created_at, tweet.text.encode('utf-8')])
 
-# Print 5 tweets (JSON)
-print(testDataSet[0:4])
+# To run on headless pi, via SSH that won't terminate the script on hangup
+# screen -S bcpoli -dm bash -c 'python ./bcpoli.py'
